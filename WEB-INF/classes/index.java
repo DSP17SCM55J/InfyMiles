ca41b73e298db02119f182a4619fa3e0693c8ec5
LAPTOP-2MJRKRGJ\Milind <mmistry2@hawk.iit.edu>
2017-11-14 00:44:58 -0600

    MySQLUtil

---
+++ WEB-INF/classes/index.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.xml.sax.SAXException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.*;
+
+public class index extends HttpServlet {
+
+
+    protected void processReq(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, java.io.IOException {
+		response.setContentType("text/html");
+        java.io.PrintWriter out = response.getWriter();
+			XMLParserSAX xml = new XMLParserSAX();
+			String map = xml.processReq();
+			out.println(map);
+        out.close();
+    }     
+	
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, java.io.IOException {
+        processReq(request, response);
+		  
+    } 
+	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, java.io.IOException {
+         processReq(request, response);
+    }
+	
+	
+}
